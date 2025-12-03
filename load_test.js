a4c86240832c5cb86c658e0e938d9e5763c9f0a4
mokchhedulislam <mokchheduls46@gmail.com>
2025-12-03 15:16:55 +0600

    add load test

---
+++ load_test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2m', target: 50 },  // ramp-up to 50 users
+    { duration: '5m', target: 100 }, // peak load
+    { duration: '2m', target: 0 },   // ramp-down
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95% requests should be below 500ms
+  },
+};
+
+export default function () {
+  let res = http.get('');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response body not empty': (r) => r.body.length > 0,
+  });
+  sleep(1);
+}
