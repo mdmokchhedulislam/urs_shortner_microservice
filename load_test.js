acc4124325ef5e1cf2fc657a9b8970bfd9531031
mokchhedulislam <mokchheduls46@gmail.com>
2025-12-03 16:10:08 +0600

    hpa update

--- load_test.js
+++ load_test.js
@@ -3,8 +3,8 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 50 },  // ramp-up to 50 users
-    { duration: '5m', target: 100 }, // peak load
+    { duration: '1m', target: 500 },  // ramp-up to 50 users
+   { duration: '5m', target: 1000 }, // peak load
     { duration: '2m', target: 0 },   // ramp-down
   ],
   thresholds: {
@@ -13,7 +13,7 @@ export let options = {
 };
 
 export default function () {
-  let res = http.get('');
+  let res =http.get('http://a7a15bda690ef4c019c2108d1eb7e75e-697610890.us-east-1.elb.amazonaws.com:5000/shorten?url=https://example.com');
   check(res, {
     'status is 200': (r) => r.status === 200,
     'response body not empty': (r) => r.body.length > 0,
